1ac924bb6e2db2811b0968d48c8829e4590b5703
Bogdan Bujor <bogdan.bujor08@gmail.com>
2020-04-24 17:28:39 +0000

    Install elastic and kibana

--- main.sh
+++ main.sh
@@ -1,3 +1,9 @@
-ls -ltrha
+apt-get install -y gnupg2
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
-ls -a
+sudo apt-get update && sudo apt-get install elasticsearch=7.6.2
+
+apt-get update && sudo apt-get install kibana=7.6.2
+
