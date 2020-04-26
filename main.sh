aa11146ef3d3c315428befa6be37d55cab048bb3
Bogdan Bujor <bogdan.bujor08@gmail.com>
2020-04-26 12:17:26 +0000

    Remove all sleep and sudo commands

--- main.sh
+++ main.sh
@@ -1,64 +1,78 @@
 #install elk prerequisites
 apt-get install -y gnupg2
-sleep 10s
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 apt-get install apt-transport-https
-sleep 5s
+apt-get update
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sleep 10s
 
 #install elastic and kibana
-sudo apt-get update && sudo apt-get install elasticsearch=7.6.2
-sleep 60s
-apt-get update && sudo apt-get install kibana=7.6.2
-sleep 60s
+apt-get update && apt-get install elasticsearch=7.6.2
+apt-get update && apt-get install kibana=7.6.2
 
 #install java
-sudo apt-get install -y software-properties-common
-sleep 10s
+apt-get install -y software-properties-common
 apt-get update
-sleep 10s
 add-apt-repository -y ppa:linuxuprising/java
-sleep 10s
-sudo apt-get update
-sleep 20s
+apt-get update
 apt install -y openjdk-11-jre-headless
-sleep 20s
 
 #install logstash and filebeat
-apt-get update && sudo apt-get install logstash=1:7.6.2-1
-sleep 60s
-apt-get update && sudo apt-get install filebeat=7.6.2
-sleep 30s
+apt-get update && apt-get install logstash=1:7.6.2-1
+apt-get update && apt-get install filebeat=7.6.2
 
 #uncomment elasticsearch hosts line
 sed -i '/elasticsearch.hosts/s/^#//g' /etc/kibana/kibana.yml
-sleep 2s
 
 #starting elk
 systemctl start elasticsearch
-sleep 30s
 systemctl start kibana
-sleep 30s
 systemctl start logstash
-sleep 30s
 
 #enable elasticsearch security
 echo 'xpack.security.enabled: true' >> /etc/elasticsearch/elasticsearch.yml
-sleep 2s
 echo 'discovery.type: single-node' >> /etc/elasticsearch/elasticsearch.yml
-sleep 2s
 
 systemctl restart elasticsearch
-sleep 30s
-systemctl stop elasticsearch
-sleep 10s
+
+#https://discuss.elastic.co/t/how-to-create-build-in-user-without-interactive-mode-or-auto/183420/2
+
+#https://discuss.elastic.co/t/how-to-set-passwords-for-built-in-users-in-batch-mode/119655/9
 
 printf "#elastic2020PBL" | /usr/share/elasticsearch/bin/elasticsearch-keystore add "bootstrap.password"
-sleep 10s
-
-curl -u elastic:"#elastic2020PBL" -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password 61' -d '{ "password”:#kibana2020PBL }'
-sleep 10s
 
 curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password' -d '{ "password":"#kibana2020PBL" }'
-sleep 10s
+
+/usr/share/kibana/bin/kibana-keystore create --allow-root
+
+printf "kibana" | /usr/share/kibana/bin/kibana-keystore add elasticsearch.username --allow-root
+
+printf "#kibana2020PBL" | /usr/share/kibana/bin/kibana-keystore add elasticsearch.password --allow-root
+
+systemctl restart kibana
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password' -d '{ "password":"#kibana2020PBL" }'
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/logstash_system/_password' -d '{ "password":"#logstash2020PBL" }'
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/apm_system/_password' -d '{ "password":"#apmsystem2020PBL" }'
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/beats_system/_password' -d '{ "password":"#beats2020PBL" }'
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/elastic/_password' -d '{ "password":"#elastic2020PBL" }'
+
+
+
+
+
+#/usr/share/kibana/bin/kibana-keystore create --allow-root
+#sleep 3s
+
+#printf "kibana" | /usr/share/kibana/bin/kibana-keystore add elasticsearch.username --allow-root
+#sleep 3s
+
+#printf "#kibana2020PBL" | /usr/share/kibana/bin/kibana-keystore add elasticsearch.password --allow-root
+#sleep 3s
+
+#systemctl restart kibana
+#sleep 30s
+
