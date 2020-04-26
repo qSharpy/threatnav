0bd68b887b6ba457aea46582cc671064967f8782
Bogdan Bujor <bogdan.bujor08@gmail.com>
2020-04-26 07:40:06 +0000

    Add elasticsearch-keystore password. Add kibana comment

--- main.sh
+++ main.sh
@@ -30,3 +30,17 @@ systemctl start logstash
 #enable elasticsearch security
 echo 'xpack.security.enabled: true' >> /etc/elasticsearch/elasticsearch.yml
 echo 'discovery.type: single-node' >> /etc/elasticsearch/elasticsearch.yml
+
+systemctl restart elasticsearch
+systemctl stop elasticsearch
+
+printf "#elastic2020PBL" | /usr/share/elasticsearch/bin/elasticsearch-keystore add "bootstrap.password"
+
+curl -u elastic:"#elastic2020PBL" -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password 61' -d '{ "password”:#kibana2020PBL }'
+
+
+curl -u elastic:#elastic2020PBL -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password' -d '{ "password”:#kibana2020PBL }'
+
+
+root@ubuntu:/home/tepa7019# curl -u elastic:"#elastic2020PBL" -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/_xpack/security/user/kibana/_password' -d '{ "password”:"#kibana2020PBL" }'
+{"error":{"root_cause":[{"type":"json_parse_exception","reason":"Unexpected character ('#' (code 35)): was expecting a colon to separate field name and value\n at [Source: org.elasticsearch.transport.netty4.ByteBufStreamInput@10613a2b; line: 1, column: 18]"}],"type":"json_parse_exception","reason":"Unexpected character ('#' (code 35)): was expecting a colon to separate field name and value\n at [Source: org.elasticsearch.transport.netty4.ByteBufStreamInput@10613a2b; line: 1, column: 18]"},"status":400}root@ubuntu:/home/tepa7019# 
